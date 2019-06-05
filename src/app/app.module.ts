ce69d0e613a49a065a5328eb10a580a89fb9e888
JHades <jhades.dev@gmail.com>
2019-06-05 15:12:59 +0200

    upgrade to Angular 8 and Node 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, Provider} from '@angular/core';
-import {HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {LessonsComponent} from './lessons/lessons.component';
@@ -9,17 +9,19 @@ import {routesConfig} from "./routes.config";
 import {LessonsService} from "./services/lessons.service";
 import {ReactiveFormsModule} from "@angular/forms";
 
-import {Router, RouterModule} from "@angular/router";
+import {environment} from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/shareReplay';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/observable/of';
+
+
+
+
+
+
+
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from '@angular/router';
 
 
 
